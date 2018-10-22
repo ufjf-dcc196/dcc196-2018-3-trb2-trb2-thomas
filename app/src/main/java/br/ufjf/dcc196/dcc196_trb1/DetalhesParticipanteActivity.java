5ac18ef26f01b1b4ca70aa5c66357fbce87afffe
Thomas Santos <thomasbsjf@gmail.com>
2018-10-22 17:30:34 -0300

    Protótipo da MainActivity + criação das demais activities

---
+++ app/src/main/java/br/ufjf/dcc196/dcc196_trb1/DetalhesParticipanteActivity.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc196.dcc196_trb1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetalhesParticipanteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_participante);
+    }
+}
---
+++ app/src/main/java/br/ufjf/dcc196/dcc196_trb1/EventoAdapter.java
@@ -0,0 +1,5 @@
+package br.ufjf.dcc196.dcc196_trb1;
+
+public class EventoAdapter{
+
+}
