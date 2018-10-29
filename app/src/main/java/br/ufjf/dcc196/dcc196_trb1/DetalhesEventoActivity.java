ca119bf76b78bf520160a9a603a8244d364d7474
Thomas Santos <thomasbsjf@gmail.com>
2018-10-28 21:19:09 -0300

    Detalhe Evento: Exibe dados

--- app/src/main/java/br/ufjf/dcc196/dcc196_trb1/DetalhesEventoActivity.java
+++ app/src/main/java/br/ufjf/dcc196/dcc196_trb1/DetalhesEventoActivity.java
@@ -2,13 +2,36 @@ package br.ufjf.dcc196.dcc196_trb1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class DetalhesEventoActivity extends AppCompatActivity {
-
+    EditText edt_titulo;
+    EditText edt_hora;
+    EditText edt_dia;
+    EditText edt_facilitador;
+    EditText edt_descricao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes_evento);
+        edt_titulo = (EditText)findViewById(R.id.edt_titulo_detalhe_evento);
+        edt_hora = (EditText)findViewById(R.id.edt_hora_detalhe_evento);
+        edt_dia = (EditText)findViewById(R.id.edt_data_detalhe_evento);
+        edt_facilitador = (EditText)findViewById(R.id.edt_facilitador_detalhe_evento);
+        edt_descricao = (EditText)findViewById(R.id.edt_descricao_detalhe_evento);
+
+        verificaParametro();
+    }
+    private void verificaParametro(){
+        Bundle bundle = getIntent().getExtras();
+        if(bundle!=null && bundle.containsKey("EVENTO")){
+            Evento evento = (Evento)bundle.getSerializable("EVENTO");
+            edt_titulo.setText(evento.getTitulo());
+            edt_hora.setText(evento.getHora());
+            edt_dia.setText(evento.getDia());
+            edt_facilitador.setText(evento.getFacilitador());
+            edt_descricao.setText(evento.getDescricao());
+        }
     }
 }
--- app/src/main/java/br/ufjf/dcc196/dcc196_trb1/EventoAdapter.java
+++ app/src/main/java/br/ufjf/dcc196/dcc196_trb1/EventoAdapter.java
@@ -1,6 +1,9 @@
 package br.ufjf.dcc196.dcc196_trb1;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,7 +26,7 @@ public class EventoAdapter extends RecyclerView.Adapter<EventoAdapter.ViewHolder
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
 
         View view = inflater.inflate(R.layout.lista_eventos, parent, false);
-        ViewHolderEvento holderEvento = new ViewHolderEvento(view);
+        ViewHolderEvento holderEvento = new ViewHolderEvento(view, parent.getContext());
 
         return holderEvento;
     }
@@ -45,10 +48,22 @@ public class EventoAdapter extends RecyclerView.Adapter<EventoAdapter.ViewHolder
 
         public TextView txt_evento;
 
-        public ViewHolderEvento(View itemView) {
+        public ViewHolderEvento(View itemView, final Context context) {
             super(itemView);
-
             txt_evento = (TextView)itemView.findViewById(R.id.txt_titulo_evento_lista);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(dados.size()>0){
+                        Evento evento = dados.get(getLayoutPosition());
+
+                        Intent intent = new Intent(context, DetalhesEventoActivity.class);
+                        intent.putExtra("EVENTO", evento);
+                        ((AppCompatActivity)context).startActivityForResult(intent,4);
+                    }
+                }
+            });
+
         }
     }
 }
